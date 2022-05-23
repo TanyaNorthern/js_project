34d0c0f294be237a7856d4671b85bfbb463ef5a6
irlandskaya.tatyana@yandex.ru <irlandskaya.tatyana@yandex.ru>
2022-05-23 13:09:31 +0300

    hw1 commit

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 const goods = [
-  { title: 'Shirt', price: 150 },
-  { title: 'Socks', price: 50 },
-  { title: 'Jacket', price: 350 },
-  { title: 'Shoes', price: 250 },
+    { title: 'Shirt', price: 150 },
+    { title: 'Socks', price: 50 },
+    { title: 'Jacket', price: 350 },
+    { title: 'Shoes', price: 250 },
+    { title: 'Jeans' },
 ];
 
-const renderGoodsItem = (title, price) => {
-  return `
-    <div class="goods-item">
+const renderGoodsItem = (title = '', price = 0) =>
+    `<div class="goods-item">
       <h3>${title}</h3>
       <p>${price}</p>
-    </div>
-  `;
-};
+    </div>`;
 
-const renderGoodsList = (list) => {
-  let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-  document.querySelector('.goods-list').innerHTML = goodsList;
-}
+const renderGoodsList = (list = []) =>
+    document.querySelector('.goods-list')
+    .innerHTML = list
+    .map(item => renderGoodsItem(item.title, item.price))
+    .reduce((first, next) => first + next, '');
 
 renderGoodsList(goods);
